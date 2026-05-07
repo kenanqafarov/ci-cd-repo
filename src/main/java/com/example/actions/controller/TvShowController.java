fc1699a9d704f516b0950492291fc2590f5cdd66
kenanqafarov <qafarovkenan06@gmail.com>
2026-05-07 17:48:43 +0400

    Done Fixing

--- src/main/java/com/example/actions/controller/TvShowController.java
+++ src/main/java/com/example/actions/controller/TvShowController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+// For testing 
+sdgdlksdglsdg
 @RestController
 @RequestMapping("/tvshows")
 public class TvShowController {
