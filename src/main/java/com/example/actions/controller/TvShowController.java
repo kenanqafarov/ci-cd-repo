f08a03c8343a8fa5552c5461da4a1aee4de8062b
kenanqafarov <qafarovkenan06@gmail.com>
2026-05-11 16:38:52 +0400

    Feat: Bonus tasked added

--- src/main/java/com/example/actions/controller/TvShowController.java
+++ src/main/java/com/example/actions/controller/TvShowController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-// For testing 
-sdgdlksdglsdg
+
 @RestController
 @RequestMapping("/tvshows")
 public class TvShowController {
